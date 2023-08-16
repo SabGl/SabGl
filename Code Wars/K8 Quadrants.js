1354b733c4cabf090fbdbfcd95d0bbc1b0925d75
SabGl <92020793+SabGl@users.noreply.github.com>
2023-08-16 20:24:39 +0200

    Add files via upload

---
+++ Code Wars/K8 Quadrants.js
@@ -0,0 +1,35 @@
+//need find part of Euclidian plane
+
+function quadrant(x, y) {
+    if(x > 0 && y > 0){
+        return 1
+    }
+    else if(x < 0 && y > 0){
+        return 2
+    }
+    else if(x < 0 && y < 0){
+        return 3
+    }
+    else {
+        return 4
+    }
+}
+
+//TEST
+
+const {assert} = require("chai");
+describe("Fixed", () => {
+    const tests = {
+        Example: [
+            [1, 2, 1], [3, 5, 1], [-10, 100, 2],
+            [-1, -9, 3], [19, -56, 4]
+        ]
+    };
+    for (var k of Object.keys(tests)) {
+        it(k, () => {
+            for (var t of tests[k]) {
+                assert.strictEqual(quadrant(t[0], t[1]), t[2]);
+            }
+        });
+    }
+});
