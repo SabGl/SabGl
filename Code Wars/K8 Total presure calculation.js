350fdcc38824807652f4c77f76ca01a86bee7d7a
Savelii <gosavelii@gmail.com>
2023-09-14 14:51:04 +0200

    Total pressure chellenge

---
+++ Code Wars/K8 Total presure calculation.js
@@ -0,0 +1,6 @@
+const solution = (M1, M2, m1, m2, V, T) => {
+    const R = 0.082;
+    return pTotal = (((m1/M1) + (m2/M2)) * R * (273.15 + T)) / V;
+}
+
+//https://www.codewars.com/kata/5b7ea71db90cc0f17c000a5a
