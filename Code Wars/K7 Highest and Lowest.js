4b77f82fa0853d16078d34e846f3248cf27cbb02
Savelii <gosavelii@gmail.com>
2023-09-22 14:52:02 +0200

    K7 Highest and Lowest task
    When we solving this task, we should use split() and Math.max and Math.min()

---
+++ Code Wars/K7 Highest and Lowest.js
@@ -0,0 +1,8 @@
+const highAndLow = (numbers) => {
+    const arrNum = numbers.split(' ')
+    return Math.max(...arrNum) + ' ' + Math.min(...arrNum);
+}
+
+
+//https://www.codewars.com/kata/554b4ac871d6813a03000035/train/javascript
+
